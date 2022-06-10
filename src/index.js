cce6c357644cf7c5391eecc71e9c8cf211c9d63b
dmvict <dm.vict.kr@gmail.com>
2022-06-10 12:09:26 +0300

    Update action, use inputs with defaults resolved from expressions

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ const core = require( '@actions/core' );
 
 try
 {
-  const value = core.getInput( 'value' );
-  const boolDefault = core.getBooleanInput( 'bool_default' );
-  const bool = core.getBooleanInput( 'bool' );
+  const environment = core.getInput( 'environment' );
+  if( environment !== 'test' )
+  throw Error( 'Unexpected value of environment variable `TEST`' );
 
-  if( !value )
-  throw Error( 'Expected option {-value-}' );
-  if( boolDefault !== true )
-  throw Error( 'Expected default bool value {-bool_default-}' );
-  if( bool !== true && bool !== false )
-  throw Error( 'Unexpected bool value {-bool-}' );
+  const github_string = core.getInput( 'github_string' );
+  if( github_string !== '90' )
+  throw Error( 'Unexpected value of github context value `retention_days`' );
+
+  const github_object = core.getInput( 'github_object' );
+  if( github_object === undefined )
+  throw Error( 'Unexpected value of github context value `event.ref`' );
+
+  const exp_with_env = core.getBooleanInput( 'exp_with_env' );
+  if( !exp_with_env )
+  throw Error( 'Unexpected value of environment variable `TEST`' );
+
+  const exp_with_env_and_github = core.getBooleanInput( 'exp_with_env_and_github' );
+  if( exp_with_env_and_github )
+  throw Error( 'Environment variable `TEST` should be differ to github context variable `retention_days`' );
 }
 catch( error )
 {
