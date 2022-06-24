16cc287c4ddffd2e3cbf599fa892bc7e15724c07
dmvict <dm.vict.kr@gmail.com>
2022-06-24 10:55:40 +0300

    Update action, use default inputs from `job` context

--- src/index.js
+++ src/index.js
@@ -2,25 +2,20 @@ const core = require( '@actions/core' );
 
 try
 {
-  const environment = core.getInput( 'environment' );
-  if( environment !== 'test' )
-  throw Error( 'Unexpected value of environment variable `TEST`' );
+  const status = core.getInput( 'status' );
+  if( status !== 'success' )
+  throw Error( 'Unexpected value of job status' );
 
-  const github_string = core.getInput( 'github_string' );
-  if( github_string !== '90' )
-  throw Error( 'Unexpected value of github context value `retention_days`' );
+  const container_network = core.getInput( 'container_network' );
+  if( container_network.length === 0 )
+  throw Error( 'Unexpected container network' );
 
-  const github_object = core.getInput( 'github_object' );
-  if( github_object === undefined )
-  throw Error( 'Unexpected value of github context value `event.ref`' );
+  const services_postgres_network = core.getInput( 'services_postgres_network' );
+  if( services_postgres_network.length === 0 )
+  throw Error( 'Unexpected sevice `postgres` network' );
 
-  const exp_with_env = core.getBooleanInput( 'exp_with_env' );
-  if( !exp_with_env )
-  throw Error( 'Unexpected value of environment variable `TEST`' );
-
-  const exp_with_env_and_github = core.getBooleanInput( 'exp_with_env_and_github' );
-  if( exp_with_env_and_github )
-  throw Error( 'Environment variable `TEST` should be differ to github context variable `retention_days`' );
+  if( container_network !== services_postgres_network )
+  throw Error( 'Container network and sevice `postgres` network should be equal.' );
 }
 catch( error )
 {
