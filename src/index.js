f0219aae181b200470f2b17711b74a03d5e6da36
dmvict <dm.vict.kr@gmail.com>
2022-06-27 09:54:04 +0300

    more

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ try
 {
   const matrix = core.getInput( 'matrix' );
   console.log( matrix );
+  console.log( typeof matrix );
+  const job = core.getInput( 'job' );
+  console.log( typeof job );
+  const github = core.getInput( 'github' );
+  console.log( github );
+  console.log( typeof github );
 }
 catch( error )
 {
