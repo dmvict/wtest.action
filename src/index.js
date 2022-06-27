57c2e03c876a5d1c40863e1823f6c5faf7f38e11
dmvict <dm.vict.kr@gmail.com>
2022-06-27 09:39:24 +0300

    matrix

--- src/index.js
+++ src/index.js
@@ -2,20 +2,8 @@ const core = require( '@actions/core' );
 
 try
 {
-  const status = core.getInput( 'status' );
-  if( status !== 'success' )
-  throw Error( 'Unexpected value of job status' );
-
-  const container_network = core.getInput( 'container_network' );
-  if( container_network.length === 0 )
-  throw Error( 'Unexpected container network' );
-
-  const services_postgres_network = core.getInput( 'services_postgres_network' );
-  if( services_postgres_network.length === 0 )
-  throw Error( 'Unexpected sevice `postgres` network' );
-
-  if( container_network !== services_postgres_network )
-  throw Error( 'Container network and sevice `postgres` network should be equal.' );
+  const matrix = core.getInput( 'matrix' );
+  console.log( matrix );
 }
 catch( error )
 {
