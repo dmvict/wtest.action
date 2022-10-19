d1e75133426c22ae7095556ddc86b3d95567cbf1
dmvict <dm.vict.kr@gmail.com>
2022-10-19 08:59:24 +0300

    Update action, setup action to check multiline input

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ const core = require( '@actions/core' );
 
 try
 {
-  const matrix = core.getInput( 'matrix' );
-  const matrixParsed = JSON.parse( matrix );
-  const matrixKeys = Object.keys( matrixParsed );
-  if( matrixKeys.length !== 1 && matrixKeys[ 0 ] !== 'os' )
-  throw Error( 'Unexpected value of matrix context' );
-
-  const matrix_os = core.getInput( 'matrix_os' );
-  if( matrixParsed.os !== matrix_os )
-  throw Error( 'Matrix context and matrix field `os` has different content' );
-  if( ![ 'ubuntu-latest', 'windows-latest', 'macos-latest' ].includes( matrix_os ) )
-  throw Error( 'Unexpected value in `matrix.os`' );
+  const multiline = core.getMultilineInput( 'multiline' );
+  if( multiline.length <= 1 )
+  throw Error( 'Expected multiline input with several lines.' );
+  const string = core.getInput( 'string' );
+  if( string !== '|' )
+  throw Error( 'Expected string with value `|`.' );
 }
 catch( error )
 {
