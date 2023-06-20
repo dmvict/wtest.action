93942cf1f1a499e4a1c95fc9b80eaef06e5d4492
dmvict <dm.vict.kr@gmail.com>
2023-06-20 15:27:53 +0300

    Update action, setup output in action

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ const core = require( '@actions/core' );
 
 try
 {
-  const multiline = core.getMultilineInput( 'multiline' );
-  if( multiline.length <= 1 )
-  throw Error( 'Expected multiline input with several lines.' );
-  const string = core.getInput( 'string' );
-  if( string !== '|' )
-  throw Error( 'Expected string with value `|`.' );
+  core.setOutput( 'test', 'foo_bar_baz' );
 }
 catch( error )
 {
