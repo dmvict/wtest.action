56c4da1ca37c714f0f5ac4f6a3c5cc46b2b63507
dmvict <dm.vict.kr@gmail.com>
2024-04-30 08:48:04 +0300

    ...

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ readonly bool_like="$2"
 echo "string=$string"
 echo "bool-like=$bool_like"
 
-echo "string=$string" >> "$GITHUB_OUTPUT"
-echo "bool-like=$bool_like" >> "$GITHUB_OUTPUT"
+echo "string=$string" >> $GITHUB_OUTPUT
+echo "bool-like=$bool_like" >> $GITHUB_OUTPUT
