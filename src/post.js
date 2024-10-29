1f29a1afde6a3fc6439b58c30e994393ab61ed54
dmvict <dm.vict.kr@gmail.com>
2024-10-29 11:14:30 +0200

    save state

--- src/post.js
+++ src/post.js
@@ -5,6 +5,10 @@ try
   const value = Number( core.getInput( 'post_value' ) );
   if( !value )
   throw new Error( 'The post value {-post_value-} doesn\'t exist.' )
+
+  core.saveState('post_value', value);
+  core.saveState('value', value);
+  core.saveState('another', value);
 }
 catch( error )
 {
